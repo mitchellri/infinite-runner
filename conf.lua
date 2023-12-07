c2e5a3f04d66e3b578471aa42d9c1bbc0b49273a
mitchellri <mitchell.rivett@hotmail.com>
2023-12-07 05:39:59 -0800

    Include, use Yonder library for state management

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.title = "Infinite Runner"
+    t.version = "11.5"
+
+    -- Debug
+    t.console = true
+end
---
+++ screens/GameScreen.lua
@@ -0,0 +1,80 @@
+-- game = require "game"
+chr = require("/character")
+
+--scalewidth=love.graphics.getWidth()
+--scaleheight=love.graphics.getHeight()
+
+-- STATE = game
+
+--	LOAD	--
+--	Called first whenever the game loads
+
+local screen = {}
+
+world = love.physics.newWorld( 0, 9.8, false )
+o = chr.new(world) --o is our object
+function screen:Load(ScreenManager)
+	--	Initial graphics
+	love.graphics.setBackgroundColor(0,0,0,0)
+	--love.window.setIcon(love.graphics.newImage("Images/mainFish/mainFish0000.png"):getData())
+	love.keyboard.setKeyRepeat(true)
+	-- STATE.load()
+	-- world = love.physics.newWorld( 0, 9.8, false )
+	-- o = chr.new(world) --o is our object
+	-- o:draw() -- pass itself as hidden parameter 
+
+end
+
+--	UPDATE	--
+--	Updates every change in time/frame
+function screen:Update( dt )
+	-- STATSE.update(dt)
+	world:update(dt)
+end
+
+function screen:Draw()
+	-- STATE.draw()
+	-- chr.draw()
+	o:draw() -- pass itself as hidden parameter 
+end
+
+--	RESIZE	--
+--	Called when you want to do resizes
+function screen:Resize( w, h )
+
+end
+
+--	KEYPRESSED	--
+--
+function screen:KeyPressed( key, isrepeat )
+	-- STATE.keypressed( key, isrepeat )
+end
+
+--	KEYRELEASED	--
+--
+function screen:KeyReleased( key )
+	-- if STATE.keyreleased( ) then
+		-- STATE.keyreleased( key, isrepeat )
+	-- end
+end
+
+--	MOUSEPRESSED	--
+--
+function screen:MousePressed( x, y, button )
+	-- STATE.mousepressed( key, isrepeat )
+end
+
+--	MOUSERELEASED	--
+--
+function screen:MouseReleased( x, y, button )
+	-- STATE.mousereleased( key, isrepeat )
+end
+
+--	QUIT	--
+--	Called when exited (are you sure you want to quit?)
+function screen:Quit()
+	love.event.quit()
+end
+
+return screen
+
