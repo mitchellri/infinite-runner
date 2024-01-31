f3ef5411aaa700e635fd2b27c811eee1495680c6
mitchellri <mitchell.rivett@hotmail.com>
2024-01-30 19:00:42 -0800

    Draw angle at correct origin, place rectangle to match body origin

--- character.lua
+++ character.lua
@@ -25,31 +25,34 @@
 		newcharacter = {}
 		newcharacter.width = 30
 		newcharacter.height = 100
+		newcharacter.color = {1, 1, 1}
 
 		local lx = love.graphics.getWidth() / 2
 		local ly = love.graphics.getHeight() / 2
 		newcharacter.body = love.physics.newBody( world, lx, ly, "dynamic")
-		newcharacter.shape = love.physics.newRectangleShape( lx, ly, newcharacter.width, newcharacter.height, 0 )
-		newcharacter.fixture = love.physics.newFixture( newcharacter.body, newcharacter.shape, 1 )
+		--[[
+			The origin of rectangle is the center of the rectangle in this case
+			Move the rectangle so the top left of the rectangle is at the origin of the body
+		]]
+		local shape = love.physics.newRectangleShape( newcharacter.width/2, newcharacter.height/2, newcharacter.width, newcharacter.height, 0 )
+		newcharacter.fixture = love.physics.newFixture( newcharacter.body, shape, 1 ) -- Shape is copied not referenced
 		function newcharacter:draw() --(pass in self- hidden newcharacter)
-			-- love.graphics.push()
-			local dx, dy = self.body:getX() + self.width / 2, self.body:getY() + self.height / 2
-			-- love.graphics.translate(dx, dy)
-			-- love.graphics.rotate(1)
-			
-			love.graphics.push()
-			love.graphics.translate(dx, dy)
-			love.graphics.rotate(self.body:getAngle())
-			
+			love.graphics.setColor(self.color)
 
-			--where I use the char to show on the screen
-			love.graphics.rectangle("fill", 
-			0,--because of : no longer need newcharacter.body:getY()
-			0,
-			self.width, 
+			-- Move the coordinate system origin (0, 0) to the location of the physical body
+			love.graphics.translate(self.body:getX(), self.body:getY())
+			-- Rotate the coordinate system (0, 0) by the rotation of the physical body
+			love.graphics.rotate(self.body:getAngle()) -- body:getAngle returns the angle relative to the origin of the body
+
+			-- Draw a regular rectangle at the now, translated and rotated coordinate system
+			love.graphics.rectangle("fill",
+			0,0,
+			self.width,
 			self.height)
-			-- love.graphics.translate(-dx, -dy)
-			love.graphics.pop()
+			
+			-- Return the coordinate system back to its default settings
+			love.graphics.origin()
+			love.graphics.setColor(1, 1, 1)
 		end 
 
 		return newcharacter
@@ -60,24 +63,34 @@
 		newcharacter = {}
 		newcharacter.width = 1000
 		newcharacter.height = 30
+		newcharacter.color = {1, 0, 0}
 
 		local lx = 0 --screen
 		local ly = love.graphics.getHeight()-newcharacter.height/2
 		newcharacter.body = love.physics.newBody( world, lx, ly, "static")
-		newcharacter.shape = love.physics.newRectangleShape( lx, ly, newcharacter.width, newcharacter.height, 0 )
-		newcharacter.fixture = love.physics.newFixture( newcharacter.body, newcharacter.shape, 1 )
+		--[[
+			The origin of rectangle is the center of the rectangle in this case
+			Move the rectangle so the top left of the rectangle is at the origin of the body
+		]]
+		local shape = love.physics.newRectangleShape( newcharacter.width/2, newcharacter.height/2, newcharacter.width, newcharacter.height, 0 )
+		newcharacter.fixture = love.physics.newFixture( newcharacter.body, shape, 1 ) -- Shape is copied not referenced
 		function newcharacter:draw() --(pass in self- hidden newcharacter)
-			-- love.graphics.push()
-			--where I use the char to show on the screen
-			love.graphics.setColor(1, 0, 0)
-			love.graphics.rectangle("fill", 
-			self.body:getX(),--because of : no longer need newcharacter.body:getY()
-			self.body:getY(),
-			self.width, 
-			self.height)
-			love.graphics.setColor(1, 1, 1)
-			-- love.graphics.pop()
+			love.graphics.setColor(self.color)
 
+			-- Move the coordinate system origin (0, 0) to the location of the physical body
+			love.graphics.translate(self.body:getX(), self.body:getY())
+			-- Rotate the coordinate system (0, 0) by the rotation of the physical body
+			love.graphics.rotate(self.body:getAngle()) -- body:getAngle returns the angle relative to the origin of the body
+
+			-- Draw a regular rectangle at the now, translated and rotated coordinate system
+			love.graphics.rectangle("fill",
+			0,0,
+			self.width,
+			self.height)
+			
+			-- Return the coordinate system back to its default settings
+			love.graphics.origin()
+			love.graphics.setColor(1, 1, 1)
 		end 
 
 		return newcharacter
