253b8b7d77e76acbfd954e2c10c752b43aaf3018
mitchellri <mitchell.rivett@hotmail.com>
2024-01-30 22:27:37 -0800

    Added inheritance, cleaned code

--- objects.lua
+++ objects.lua
@@ -1,98 +1,82 @@
-	Objects = {}
-	Objects.Character = {}
-	Objects.Floor = {}
+--[[	BASE	]]
+--[[
+	Initializes the minimum required attributes for common objects
+	Defines common unchanging logic, and static variables
+]]
 
-	--set initial env
-	-- Objects.timer = 0
-	-- Objects.speed = 1
+Base = {}
 
-	-- function love.update( dt )
-	-- 	-- STATE.update(dt)
-	-- end
+Base.Object = { -- All objects must contain these attributes
+	color = {1, 1, 1, 1},
+	width = 0,
+	height = 0,
+	body = nil,
+	fixture = nil,
+	draw = function() end
+}
+
+Base.Rectangle = setmetatable({}, {__index = Base.Object}) -- Assign a new table, and when an index is not found in Base.Rectangle look in Base.Object
+function Base.Rectangle:draw()
+	love.graphics.setColor(self.color)
+
+	-- Move the coordinate system origin (0, 0) to the location of the physical body
+	love.graphics.translate(self.body:getX(), self.body:getY())
+	-- Rotate the coordinate system (0, 0) by the rotation of the physical body
+	love.graphics.rotate(self.body:getAngle()) -- body:getAngle returns the angle relative to the origin of the body
+
+	-- Draw a regular rectangle at the now, translated and rotated coordinate system
+	love.graphics.rectangle("fill",
+	0,0,
+	self.width,
+	self.height)
 	
-	function Objects:draw() --pass in self
-		--where I use the char to show on the screen
-		love.graphics.rectangle("fill", 0,50, 40,120)
-	end 
+	-- Return the coordinate system back to its default settings
+	love.graphics.origin()
+	love.graphics.setColor(1, 1, 1)
+end
 
-	-- function Objects:
-	-- function Objects.move(timer)
-	-- 	Objects.move += timer*speed
-	-- end
+--[[	OBJECTS	]]
+--[[
+	Objects to be used in the game
+	This is the exported table
+]]
 
-	function Objects.Character.new(world)
-		newcharacter = {}
-		newcharacter.width = 30
-		newcharacter.height = 100
-		newcharacter.color = {1, 1, 1}
+Objects = {}
+Objects.Character = {}
+Objects.Floor = {}
 
-		local lx = love.graphics.getWidth() / 2
-		local ly = love.graphics.getHeight() / 2
-		newcharacter.body = love.physics.newBody( world, lx, ly, "dynamic")
-		--[[
-			The origin of rectangle is the center of the rectangle in this case
-			Move the rectangle so the top left of the rectangle is at the origin of the body
-		]]
-		local shape = love.physics.newRectangleShape( newcharacter.width/2, newcharacter.height/2, newcharacter.width, newcharacter.height, 0 )
-		newcharacter.fixture = love.physics.newFixture( newcharacter.body, shape, 1 ) -- Shape is copied not referenced
-		function newcharacter:draw() --(pass in self- hidden newcharacter)
-			love.graphics.setColor(self.color)
+function Objects.Character.new(world, x, y)
+	local o = setmetatable({}, {__index = Base.Rectangle}) -- Create a new object - When an index isn't found in the object, look at Base.Rectangle
+	o.width = 30
+	o.height = 100
 
-			-- Move the coordinate system origin (0, 0) to the location of the physical body
-			love.graphics.translate(self.body:getX(), self.body:getY())
-			-- Rotate the coordinate system (0, 0) by the rotation of the physical body
-			love.graphics.rotate(self.body:getAngle()) -- body:getAngle returns the angle relative to the origin of the body
+	o.body = love.physics.newBody( world, x, y, "dynamic")
+	--[[
+		The origin of rectangle is the center of the rectangle in this case
+		Move the rectangle so the top left of the rectangle is at the origin of the body
+	]]
+	local shape = love.physics.newRectangleShape( o.width/2, o.height/2, o.width, o.height, 0 ) -- Shape is copied not referenced - can retrieve shape via fixture:getShape
+	o.fixture = love.physics.newFixture( o.body, shape, 1 )
 
-			-- Draw a regular rectangle at the now, translated and rotated coordinate system
-			love.graphics.rectangle("fill",
-			0,0,
-			self.width,
-			self.height)
-			
-			-- Return the coordinate system back to its default settings
-			love.graphics.origin()
-			love.graphics.setColor(1, 1, 1)
-		end 
-
-		return newcharacter
-	end
+	return o
+end
 
 
-	function Objects.Floor.new(world)
-		newcharacter = {}
-		newcharacter.width = 1000
-		newcharacter.height = 30
-		newcharacter.color = {1, 0, 0}
+function Objects.Floor.new(world, x, y, width, height)
+	local o = setmetatable({}, {__index = Base.Rectangle}) -- Create a new object - When an index isn't found in the object, look at Base.Rectangle
+	o.width = width
+	o.height = height
+	o.color = {0, 1, 0}
 
-		local lx = 0 --screen
-		local ly = love.graphics.getHeight()-newcharacter.height/2
-		newcharacter.body = love.physics.newBody( world, lx, ly, "static")
-		--[[
-			The origin of rectangle is the center of the rectangle in this case
-			Move the rectangle so the top left of the rectangle is at the origin of the body
-		]]
-		local shape = love.physics.newRectangleShape( newcharacter.width/2, newcharacter.height/2, newcharacter.width, newcharacter.height, 0 )
-		newcharacter.fixture = love.physics.newFixture( newcharacter.body, shape, 1 ) -- Shape is copied not referenced
-		function newcharacter:draw() --(pass in self- hidden newcharacter)
-			love.graphics.setColor(self.color)
+	o.body = love.physics.newBody( world, x, y, "static")
+	--[[
+		The origin of rectangle is the center of the rectangle in this case
+		Move the rectangle so the top left of the rectangle is at the origin of the body
+	]]
+	local shape = love.physics.newRectangleShape( o.width/2, o.height/2, o.width, o.height, 0 )
+	o.fixture = love.physics.newFixture( o.body, shape, 1 ) -- Shape is copied not referenced
+	
+	return o
+end
 
-			-- Move the coordinate system origin (0, 0) to the location of the physical body
-			love.graphics.translate(self.body:getX(), self.body:getY())
-			-- Rotate the coordinate system (0, 0) by the rotation of the physical body
-			love.graphics.rotate(self.body:getAngle()) -- body:getAngle returns the angle relative to the origin of the body
-
-			-- Draw a regular rectangle at the now, translated and rotated coordinate system
-			love.graphics.rectangle("fill",
-			0,0,
-			self.width,
-			self.height)
-			
-			-- Return the coordinate system back to its default settings
-			love.graphics.origin()
-			love.graphics.setColor(1, 1, 1)
-		end 
-
-		return newcharacter
-	end
-
-	return Objects
+return Objects
--- screens/GameScreen.lua
+++ screens/GameScreen.lua
@@ -1,80 +1,29 @@
--- game = require "game"
-objects = require("objects")
-
---scalewidth=love.graphics.getWidth()
---scaleheight=love.graphics.getHeight()
-
--- STATE = game
-
---	LOAD	--
---	Called first whenever the game loads
+local objects = require("objects")
 
 local screen = {}
 
-world = love.physics.newWorld( 0, 9.8, false )
-o = objects.Character.new(world) --o is our object //our rectangle
-floor = objects.Floor.new(world)
-function screen:Load(ScreenManager)
-	--	Initial graphics
-	love.graphics.setBackgroundColor(0,0,0,0)
-	--love.window.setIcon(love.graphics.newImage("Images/mainFish/mainFish0000.png"):getData())
-	love.keyboard.setKeyRepeat(true)
-	-- STATE.load()
-	-- world = love.physics.newWorld( 0, 9.8, false )
-	-- o = objects.new(world) --o is our object
-	-- o:draw() -- pass itself as hidden parameter 
+local world = nil
+local character = nil
+local floor = nil
 
+function screen:Load(ScreenManager)
+	love.graphics.setBackgroundColor(0,0,0,0)
+	love.keyboard.setKeyRepeat(true)
+
+	world = love.physics.newWorld( 0, 9.8, false )
+	character = objects.Character.new(world, love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+	floor = objects.Floor.new(world, 0, love.graphics.getHeight()-50/2, love.graphics.getWidth(), 50)
 end
 
---	UPDATE	--
---	Updates every change in time/frame
 function screen:Update( dt )
-	-- STATSE.update(dt)
 	world:update(dt)
 end
 
 function screen:Draw()
-	-- STATE.draw()
-	-- objects.draw()
 	floor:draw()
-	o:draw() -- pass itself as hidden parameter 
-	
+	character:draw()
 end
 
---	RESIZE	--
---	Called when you want to do resizes
-function screen:Resize( w, h )
-
-end
-
---	KEYPRESSED	--
---
-function screen:KeyPressed( key, isrepeat )
-	-- STATE.keypressed( key, isrepeat )
-end
-
---	KEYRELEASED	--
---
-function screen:KeyReleased( key )
-	-- if STATE.keyreleased( ) then
-		-- STATE.keyreleased( key, isrepeat )
-	-- end
-end
-
---	MOUSEPRESSED	--
---
-function screen:MousePressed( x, y, button )
-	-- STATE.mousepressed( key, isrepeat )
-end
-
---	MOUSERELEASED	--
---
-function screen:MouseReleased( x, y, button )
-	-- STATE.mousereleased( key, isrepeat )
-end
-
---	QUIT	--
---	Called when exited (are you sure you want to quit?)
 function screen:Quit()
 	love.event.quit()
 end
