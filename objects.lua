f92e2d8c361667e9cef1cc8554d7010140638f98
mitchellri <mitchell.rivett@hotmail.com>
2024-04-10 14:38:28 -0700

    Change obstacle and player to circles, obstacles keep vertical velocity

--- objects.lua
+++ objects.lua
@@ -1,5 +1,7 @@
 require('/lib/AnAL')
 
+local bodyScale = 0.95 -- Generally we want the collidable area to be smaller than what we see, to avoid frustration when playing
+
 --[[	TYPE	]]
 Type = {
 	obstacle = 1
@@ -27,7 +29,6 @@ Base.Object = { -- All objects must contain these attributes
 }
 
 Base.Rectangle = setmetatable({}, {__index = Base.Object}) -- Assign a new table, and when an index is not found in Base.Rectangle look in Base.Object
-
 function Base.Rectangle:draw()
 	love.graphics.setColor(self.color)
 	-- Move the coordinate system origin (0, 0) to the location of the physical body
@@ -46,16 +47,36 @@ function Base.Rectangle:draw()
 	love.graphics.setColor(1, 1, 1, 1)
 end
 
-Base.Obstacle = setmetatable({}, {__index = Base.Object}) -- Assign a new table, and when an index is not found in Base.Obstacle look in Base.Object
+Base.Circle = setmetatable({}, {__index = Base.Object}) -- Assign a new table, and when an index is not found in Base.Circle look in Base.Object
+function Base.Circle:draw()
+	love.graphics.setColor(self.color)
+
+	-- Circle is drawn from the center, compared to rectangles which are drawn from the top left
+	local shape = self.fixture:getShape()
+	local cx, cy = self.body:getWorldPoints(shape:getPoint())
+	-- WARNING: Rotation is not accounted for in this draw
+	love.graphics.circle("fill", cx, cy, shape:getRadius())
+	
+	-- Return the coordinate system back to its default settings
+	love.graphics.origin()
+	love.graphics.setColor(1, 1, 1, 1)
+end
+
+Base.Obstacle = setmetatable({}, {__index = Base.Circle}) -- Assign a new table, and when an index is not found in Base.Obstacle look in Base.Object
 Base.Obstacle.image = love.graphics.newImage("images/objects/rock.png")
-Base.Obstacle.width = 61
-Base.Obstacle.height = 75
+Base.Obstacle.radius = math.min(Base.Obstacle.image:getHeight(), Base.Obstacle.image:getWidth()) / 2 * bodyScale
 Base.Obstacle.color = {0, 1, 0}
 function Base.Obstacle:update(dt)
-	self.body:setLinearVelocity(Objects.Speed, 0) -- At this line of code, "Objects" has not been defined yet - but since it's in a function that gets called after it's defined, it will find the reference
+	local vx, vy = self.body:getLinearVelocity()
+	self.body:setLinearVelocity(Objects.Speed, vy)
 end
 function Base.Obstacle:draw()
-	love.graphics.draw(self.image, self.body:getX(), self.body:getY(), self.body:getAngle())
+	-- Body origin is the center of the circle, animation origin is the top left of the animation
+	-- Subtract radius to align with top left of the body as if it were a rectangle
+	-- Add the diference between the body width and the animation
+	-- This draws the center of the image at the center of the body
+	-- WARNING: Rotation is not correctly accounted for in this draw
+	love.graphics.draw(self.image, self.body:getX() - self.radius + (self.radius*2 - self.image:getWidth()) / 2, self.body:getY() - self.radius + (self.radius*2 - self.image:getHeight()) / 2, self.body:getAngle())
 end
 
 --[[	OBJECTS	]]
@@ -72,19 +93,19 @@ Objects.Rock = {}
 
 function Objects.Character.new(world, x, y)
 	local o = setmetatable({}, {__index = Base.Object}) -- Create a new object - When an index isn't found in the object, look at Base.Object
-	o.scale = 5
-	o.width = 21 * o.scale
-	o.height = 16 * o.scale
-	o.jumpVelocity = 1200/2
-	o.body = love.physics.newBody( world, x, y, "dynamic")
 	o.animation = {}
 	o.animation.walk = newAnimation(love.graphics.newImage("images/Player/walk.png"), 21, 16, 0.1, 8)
 	o.animation.current = o.animation.walk
+	o.scale = 5
+	o.radius = math.min(o.animation.current:getWidth() * o.scale, o.animation.current:getHeight() * o.scale) / 2 * bodyScale
+	o.jumpVelocity = 1200/2
+	o.body = love.physics.newBody( world, x, y, "dynamic")
+	o.body:setFixedRotation(true)
 	--[[
-		The origin of rectangle is the center of the rectangle in this case
-		Move the rectangle so the top left of the rectangle is at the origin of the body
+		The origin of a circle "shape" is at the top left (as if it were a rectangle)
+		The top left of the circle "shape" is at the origin of the "body" (0, 0) locally without translating it
 	]]
-	local shape = love.physics.newRectangleShape( o.width/2, o.height/2, o.width, o.height, 0 ) -- Shape is copied not referenced - can retrieve shape via fixture:getShape
+	local shape = love.physics.newCircleShape( 0, 0, o.radius ) -- Shape is copied not referenced - can retrieve shape via fixture:getShape
 	o.fixture = love.physics.newFixture( o.body, shape, 1 )
 	o.fixture:setUserData(o) -- Set the data that gets passed when a collision is detected (Required for collision detection)
 
@@ -119,7 +140,12 @@ function Objects.Character.new(world, x, y)
 	end
 
 	function o:draw()
-		self.animation.current:draw(self.body:getX(), self.body:getY(), self.body:getAngle(), self.scale)
+		-- Body origin is the center of the circle, animation origin is the top left of the animation
+		-- Subtract radius to align with top left of the body as if it were a rectangle
+		-- Add the diference between the body width and the animation
+		-- This draws the center of the image at the center of the body
+		-- WARNING: Rotation is not correctly accounted for in this draw
+		self.animation.current:draw(self.body:getX() - self.radius + (self.radius*2 - self.animation.current:getWidth() * self.scale) / 2, self.body:getY() - self.radius + (self.radius*2 - self.animation.current:getHeight() * self.scale) / 2, self.body:getAngle(), self.scale)
 	end
 
 	return o
@@ -134,8 +160,8 @@ function Objects.Floor.new(world, x, y, width, height)
 
 	o.body = love.physics.newBody( world, x, y, "static")
 	--[[
-		The origin of rectangle is the center of the rectangle in this case
-		Move the rectangle so the top left of the rectangle is at the origin of the body
+		The origin of a rectangle "shape" is at the center
+		Move the rectangle "shape" so the top left of the rectangle "shape" is at the origin of the "body" (0, 0) locally
 	]]
 	local shape = love.physics.newRectangleShape( o.width/2, o.height/2, o.width, o.height, 0 )
 	o.fixture = love.physics.newFixture( o.body, shape, 1 ) -- Shape is copied not referenced
@@ -148,11 +174,12 @@ function Objects.Rock.new(world, x, y)
 	local o = setmetatable({}, {__index = Base.Obstacle}) -- Create a new object - When an index isn't found in the object, look at Base.Obstacle
 	
 	o.body = love.physics.newBody( world, x, y, "dynamic")
+	o.body:setFixedRotation( true )
 	--[[
-		The origin of rectangle is the center of the rectangle in this case
-		Move the rectangle so the top left of the rectangle is at the origin of the body
+		The origin of a circle "shape" is at the top left (as if it were a rectangle)
+		The top left of the circle "shape" is at the origin of the "body" (0, 0) locally without translating it
 	]]
-	local shape = love.physics.newRectangleShape( o.width/2, o.height/2, o.width, o.height, 0 )
+	local shape = love.physics.newCircleShape( 0, 0, o.radius )
 	o.fixture = love.physics.newFixture( o.body, shape, 1 ) -- Shape is copied not referenced
 	o.fixture:setUserData(o) -- Set the data that gets passed when a collision is detected (Required for collision detection)
 
