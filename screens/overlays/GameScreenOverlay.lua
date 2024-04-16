b66e97015cdaa6be1b811e2877775e7361e96acb
NingYetian <1786232481@qq.com>
2024-04-15 20:04:49 -0700

    add score for game based on time

--- screens/overlays/GameScreenOverlay.lua
+++ screens/overlays/GameScreenOverlay.lua
@@ -1,22 +1,29 @@
 local overlay = {
     isVisible = true,
     isActive = true,
-    time = 0
+    time = 0,
+    score = 0
 }
 
 function overlay:load()
   self.isVisible = true
   self.isActive = true
   self.time = 0
+  self.score = 0
 end
 
 function overlay:draw()
     love.graphics.print("Time: " .. tostring(math.floor(self.time)) .. "s")
+    love.graphics.print("Score: " .. tostring(math.floor(self.score)) .. " ", 0, 10)
 end
 
 function overlay:update(dt)
   if self.isActive then
     self.time = self.time + dt
+  end
+
+  if self.isActive then
+    self.score = self.score + self.time/2*math.exp(0.005)
   end
 end
 
